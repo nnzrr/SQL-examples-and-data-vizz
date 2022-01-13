49efc5f48e0b3d48aadc6c98847d073251a7186a
Navarudin Ahmad <43535279+nnzrr@users.noreply.github.com>
2022-01-13 10:54:55 +0700

    Update basics1.sql

--- basics SQL/basics1.sql
+++ basics SQL/basics1.sql
@@ -1,1 +1,7 @@
-SELECT DISTINCT make, model, MAX(msrp) FROM car GROUP BY make, model ORDER BY MAX(msrp) DESC LIMIT 50
+
+--Data Source: https://www.kaggle.com/CooperUnion/cardataset
+
+SELECT DISTINCT make, model, MAX(msrp) 
+  FROM car GROUP BY make, model 
+  ORDER BY MAX(msrp) 
+  DESC LIMIT 50
